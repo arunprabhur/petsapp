7c9ff7fb9af2753a7edad0c9470f5427f2fed92e
arunprabhur <prabhu.arun10@gmail.com>
2020-08-01 17:59:43 +0530

    updated with weather

--- main.py
+++ main.py
@@ -1,36 +1,68 @@
-from flask import Flask, render_template
-import random
+from __future__ import print_function
+#from future.standard_library import install_aliases
+#install_aliases()
 
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+import requests as core_requests
+
+# Flask app should start in global layout
 app = Flask(__name__)
 
-# list of cat images
-images = [
-   "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
-    "https://media.giphy.com/media/7Jkv02RLFYj6M/giphy.gif",
-    "https://media2.giphy.com/media/pYCHFShJaQ2OY/giphy.gif",
-    "https://media.giphy.com/media/jkSvCVEXWlOla/giphy.gif",
-    "https://meetat-thebarre.com/wp-content/uploads/2017/01/tumblr_inline_oj0n99r5NO1ro2d43_500.gif",
-    "https://i.gifer.com/YLbc.gif",
-    "http://www.sunnyskyz.com/uploads/2013/12/k8riy-cat-treat-on-head.gif"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = make_openweathermap_request(req)
+
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+    
+@app.route("/")
+def root():
+    return "Hello World"
 
 
-    ]
+def make_openweathermap_request(req):
+    result = req.get("queryResult")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
 
-@app.route('/')
-def index():
-    url = random.choice(images)
-    return render_template('index.html', url=url)
+    print('https://api.openweathermap.org/data/2.5/weather?q={}&appid=4caf7420789e9f763a81fbc58dc34e31'.format(city))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    try:
+        r = core_requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid=4caf7420789e9f763a81fbc58dc34e31'.format(city))
+        current_temp = r.json()['main']['temp']
+        current_temp = float(current_temp) - 273.0
+        current_temp = int(current_temp)
+
+        speech_response = 'Current temperature in {} is {} Celsius'.format(city, current_temp)
+    except Exception as e:
+        speech_response = 'Unable to get temperature of {}'.format(city)
+
+    return {
+        "fulfillmentText": speech_response,
+        "source": "Yahoo Weather"
+    }
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
